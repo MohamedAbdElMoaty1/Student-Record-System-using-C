a54fce8a1b62d40ffb4103749bde36bc9951e5e7
Mohamed Ahmed <138626519+MohamedAbdElMoaty1@users.noreply.github.com>
2024-05-10 21:46:05 +0300

    Add files via upload

---
+++ interface/menu.h
@@ -0,0 +1,10 @@
+#include "..\application\authintication.h"
+#include "..\application\student.h"
+#include "..\application\admin.h"
+
+#ifndef C_PROJECT_MENU_H
+#define C_PROJECT_MENU_H
+void admin_mode();
+void user_mode();
+void select_mode();
+#endif //C_PROJECT_MENU_H
