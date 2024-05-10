6a7891169d0c542636d6006f47ed2a394e98f944
Mohamed Ahmed <138626519+MohamedAbdElMoaty1@users.noreply.github.com>
2024-05-10 21:49:11 +0300

    Add files via upload

---
+++ application/admin.h
@@ -0,0 +1,15 @@
+
+#include"..\data_access\file_access.h"
+
+#include"..\data_access\data_managment.h"
+#include"..\data_access\memory_managment.h"
+#ifndef NADA_ADMIN_H
+#define NADA_ADMIN_H
+void remove_student_data();
+void edit_admin_password();
+void edit_student_grade();
+void add_student_record();
+void view_student_record();
+void view_all_students();
+
+#endif //NADA_ADMIN_H*/
---
+++ application/student.h
@@ -0,0 +1,10 @@
+#include"..\data_access\file_access.h"
+#include"..\data_access\data_managment.h"
+#include "..\data_access\memory_managment.h"
+#ifndef NADA_STUDENT_H
+#define NADA_STUDENT_H
+void view_student_data();
+void edit_student_pass();
+void edit_student_name();
+
+#endif //NADA_STUDENT_H
